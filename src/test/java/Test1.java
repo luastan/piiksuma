1f9cf4043cd5c9a947ed082d0bca02114d633308
luastan <saulbarral@gmail.com>
2019-03-28 14:31:27 +0100

    Prueba de commit para el travis

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,9 +1,16 @@
 import org.junit.Test;
 import piiksuma.database.FachadaBDD;
 
+import static org.junit.Assert.assertNotNull;
+
 public class Test1 {
     @Test
     public void whatever_test1() {
         System.out.println(FachadaBDD.getDb().test().get(0).get("nombre"));
+    }
+
+    @Test
+    public void test2_prueba() {
+        assertNotNull("lol");
     }
 }
