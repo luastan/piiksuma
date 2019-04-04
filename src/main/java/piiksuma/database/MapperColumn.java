9d780386222acc3c7d29e41cf21ea13351718a1c
luastan <saulbarral@gmail.com>
2019-04-04 13:36:40 +0200

    Ahora el Mapper recorre las claves foraneas mapeandolas
    Signed-off-by: luastan <saulbarral@gmail.com>

--- src/main/java/piiksuma/database/MapperColumn.java
+++ src/main/java/piiksuma/database/MapperColumn.java
@@ -4,11 +4,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Type;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface MapperColumn {
     String columna() default "";
     boolean pkey() default false;
-
+    Class<?> targetClass() default Object.class;
 }
