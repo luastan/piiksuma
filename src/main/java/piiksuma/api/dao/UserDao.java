4b5a928de12853ef757abe3ef3fdba07e8a2c952
Francisco Javier Cardama Santiago <francardama@gmail.com>
2019-04-04 10:06:38 +0200

    Declaración de los métodos de UserDAO

--- src/main/java/piiksuma/api/dao/UserDao.java
+++ src/main/java/piiksuma/api/dao/UserDao.java
@@ -1,4 +1,41 @@
 package piiksuma.api.dao;
 
 public class UserDao {
+
+    public void removeUser(User user, User current){
+
+    }
+
+    public User createUser(User user, User current){
+        return null;
+    }
+
+    public User updateUser(User user, User current){
+        return null;
+    }
+
+    public User getUser(User user, User current){
+        return null;
+    }
+
+    public List<Achievement> getAchievement(User user, User current){
+        return null;
+    }
+
+    public Achievement createAchievement(Achievement achievement, User current){
+        return null;
+    }
+
+    public Achievement unlockAchievement(Achievement achievement, User current){
+        return null;
+    }
+
+    public void followUser(User user, User current){
+
+    }
+
+    public void unfollowUser(User user, User current){
+
+    }
+    
 }
