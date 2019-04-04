34c630eb1953517122d59d40d5c8de11f2e5c8c0
Álvaro <alvarogoldard@gmail.com>
2019-04-04 10:15:35 +0200

    añadidos prototipos métodos PostDao

--- src/main/java/piiksuma/api/dao/MessagesDao.java
+++ src/main/java/piiksuma/api/dao/MessagesDao.java
@@ -3,6 +3,7 @@ package piiksuma.api.dao;
 import java.sql.Connection;
 
 public class MessagesDao extends AbstractDao {
+
     public MessagesDao(Connection connection) {
         super(connection);
     }
