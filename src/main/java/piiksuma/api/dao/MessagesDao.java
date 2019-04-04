2e892b042177aca8c967c14bcb2aae6b3ff2f1f6
danimf99 <danijugon@hotmail.com>
2019-04-04 10:33:38 +0200

    Creación de métodos vacíos para el DAO de mensajes.

--- src/main/java/piiksuma/api/dao/MessagesDao.java
+++ src/main/java/piiksuma/api/dao/MessagesDao.java
@@ -1,9 +1,36 @@
 package piiksuma.api.dao;
 
+import piiksuma.Usuario;
+
 import java.sql.Connection;
 
 public class MessagesDao extends AbstractDao {
     public MessagesDao(Connection connection) {
         super(connection);
     }
+
+    public void deleteMessage(Message privateMessage, Usuario currentUser) {
+
+    }
+
+    public void modifyMessage(Message privateMessage, Usuario currentUser) {
+
+    }
+
+    public void sendMessage(Message privateMessage, Usuario currentUser) {
+
+    }
+
+    public Ticket newTicket(Ticket ticker, Usuario currentUser) {
+
+    }
+
+    public void replyTicket(Ticket ticker, Usuario currentUser) {
+
+    }
+
+    public void closeTicket(Ticket ticket, Usuario currentUser) {
+
+    }
+
 }
