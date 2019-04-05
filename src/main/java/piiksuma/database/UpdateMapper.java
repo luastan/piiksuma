e3d5855fe8d26ec97db92bb212a29f2b19d17dd2
luastan <saulbarral@gmail.com>
2019-04-05 17:02:46 +0200

    Avances en el Mapper
    Signed-off-by: luastan <saulbarral@gmail.com>

---
+++ src/main/java/piiksuma/database/UpdateMapper.java
@@ -0,0 +1,17 @@
+package piiksuma.database;
+
+
+import java.sql.Connection;
+
+public class UpdateMapper<T> extends Mapper {
+
+
+    public UpdateMapper(Connection connection) {
+        super(connection);
+    }
+
+
+
+
+
+}
