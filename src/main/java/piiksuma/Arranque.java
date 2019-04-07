dab354a09507dbd078b6c2fc8a10eab822ef0e70
OswaldOswin1 <pablo.martinez.gomez0@rai.usc.es>
2019-04-07 14:06:20 +0200

    Prueba (Otra vez)

--- src/main/java/piiksuma/Arranque.java
+++ src/main/java/piiksuma/Arranque.java
@@ -6,6 +6,7 @@ public class Arranque {
     public static void main(String[] args) {
         //System.out.println("lol");
         //System.out.println(SampleFachada.getDb().test().get(0).get("nombre"));
+
         System.out.println("\n\n");
 
         System.out.println(SampleFachada.getDb().usuarios());
