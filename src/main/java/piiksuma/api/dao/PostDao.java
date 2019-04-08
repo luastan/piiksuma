7ad5af1462ccc9004139c407d64edb796396b104
Francisco Javier Cardama Santiago <francardama@gmail.com>
2019-04-08 10:24:34 +0200

    Cambios en el método createPost

--- src/main/java/piiksuma/api/dao/PostDao.java
+++ src/main/java/piiksuma/api/dao/PostDao.java
@@ -5,8 +5,6 @@ import piiksuma.database.InsertionMapper;
 import piiksuma.database.QueryMapper;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 public class PostDao extends AbstractDao {
