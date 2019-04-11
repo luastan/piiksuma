6b9d14d8ae6a839dc5255946720064df72fbeb52
alvaro <alvarogoldard@gmail.com>
2019-04-11 09:54:20 +0200

    PiikForbiddenException added

---
+++ src/main/java/piiksuma/exceptions/PiikForbiddenException.java
@@ -0,0 +1,8 @@
+package piiksuma.exceptions;
+
+public class PiikForbiddenException extends PiikDatabaseException {
+
+    public PiikForbiddenException(String message) {
+        super(message);
+    }
+}
