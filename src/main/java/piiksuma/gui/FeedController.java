294b1ff2aaf6637b3d35f809762c62ed0d5c3940
S <saulbarral@gmail.com>
2019-04-23 12:35:10 +0200

    Small tweaks to the gui
    Signed-off-by: luastan <saulbarral@gmail.com>

--- src/main/java/piiksuma/gui/FeedController.java
+++ src/main/java/piiksuma/gui/FeedController.java
@@ -1,11 +1,16 @@
 package piiksuma.gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FeedController implements Initializable {
+
+    @FXML
+    private Label superCoolLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
