0832a49eba5fd6f44c2fe2443d12a63a34dc0fa8
OswaldOswin1 <pablo.martinez.gomez0@rai.usc.es>
2019-04-25 20:09:30 +0200

    Message GUI main button now do something

--- src/main/java/piiksuma/gui/deckControllers/EventsDeckController.java
+++ src/main/java/piiksuma/gui/deckControllers/EventsDeckController.java
@@ -1,12 +1,21 @@
 package piiksuma.gui.deckControllers;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDecorator;
 import com.jfoenix.controls.JFXHamburger;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import piiksuma.exceptions.PiikInvalidParameters;
+import piiksuma.gui.ContextHandler;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,6 +27,9 @@ public class EventsDeckController extends AbstractDeckController implements Init
     @FXML
     private JFXHamburger hamburguerButton;
 
+    @FXML
+    private JFXButton m;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Common deck implementation
@@ -28,5 +40,45 @@ public class EventsDeckController extends AbstractDeckController implements Init
         FontAwesomeIconView buttonIcon = new FontAwesomeIconView(FontAwesomeIcon.BOOKMARK);
         buttonIcon.getStyleClass().add("deck-button-graphic");
         mainButton.setGraphic(buttonIcon);
+        m.setOnAction(this::handleMessageButton);
+    }
+
+
+    public void handleMessageButton(ActionEvent event) {
+        Stage searchStage = new Stage();
+
+        try {
+            ContextHandler.getContext().register("Start chat", searchStage);
+        } catch (PiikInvalidParameters e) {
+            e.printStackTrace();
+            return;
+        }
+        // Stage configuration
+        searchStage.setTitle("Start chat");
+        searchStage.setResizable(false);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/startChat.fxml"));
+        JFXDecorator decorator;
+
+        try {
+            decorator = new JFXDecorator(searchStage, loader.load(), false, false, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(decorator, 450, 800);
+
+
+        scene.getStylesheets().addAll(
+                getClass().getResource("/gui/css/global.css").toExternalForm(),
+                getClass().getResource("/gui/css/main.css").toExternalForm()
+        );
+        searchStage.initModality(Modality.WINDOW_MODAL);
+        searchStage.initOwner(ContextHandler.getContext().getStage("primary"));
+        searchStage.setScene(scene);
+        // Show and wait till it closes
+        searchStage.showAndWait();
+
     }
 }
+
