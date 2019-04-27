9dc12d8bdca03329681412d8451f683ab51db925
danimf99 <danijugon@hotmail.com>
2019-04-27 19:19:04 +0200

    MoreInfo button working for Events

--- src/main/java/piiksuma/gui/EventController.java
+++ src/main/java/piiksuma/gui/EventController.java
@@ -1,13 +1,20 @@
 package piiksuma.gui;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDecorator;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import piiksuma.Event;
 import piiksuma.api.ApiFacade;
 import piiksuma.exceptions.PiikException;
+import piiksuma.exceptions.PiikInvalidParameters;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -32,12 +39,23 @@ public class EventController implements Initializable {
     private JFXButton participate;
 
     @FXML
+    private JFXButton moreInfo;
+
+    @FXML
     private JFXButton share;
 
     private Event eventP;
 
+    private boolean moreInfoClicked;
+
     public EventController(Event event) {
         this.eventP = event;
+        moreInfoClicked = false;
+    }
+
+    public EventController(Event event, boolean moreInfoClicked) {
+        this.eventP = event;
+        this.moreInfoClicked = moreInfoClicked;
     }
 
     @Override
@@ -45,10 +63,16 @@ public class EventController implements Initializable {
 
         setEventInfo();
 
+        if (moreInfoClicked == true) {
+            moreInfo.setVisible(false);
+            return;
+        }
+
         participate.setOnAction(this::handleParticipate);
+        moreInfo.setOnAction(this::handleMoreInfo);
     }
 
-    private void setEventInfo(){
+    private void setEventInfo() {
         eventDescription.setText(eventP.getDescription());
 //        authorName.setText(eventP.getCreator().getName());
 //        authorId.setText(eventP.getCreator().getId());
@@ -56,12 +80,49 @@ public class EventController implements Initializable {
         eventLocation.setText(eventP.getLocation());
     }
 
-    private void handleParticipate(javafx.event.Event event){
-        try{
+    private void handleParticipate(javafx.event.Event event) {
+
+        try {
             ApiFacade.getEntrypoint().getInsertionFacade().participateEvent(eventP, ContextHandler.getContext().getCurrentUser());
-        }catch (PiikException e){
+        } catch (PiikException e) {
             System.out.println(e.getMessage());
             return;
         }
+    }
+
+    private void handleMoreInfo(javafx.event.Event event) {
+        Stage registerStage = new Stage();
+
+        try {
+            ContextHandler.getContext().register("moreInfo", registerStage);
+        } catch (PiikInvalidParameters e) {
+            e.printStackTrace();
+            return;
+        }
+        // Stage configuration
+        registerStage.setTitle("Event");
+        registerStage.setResizable(false);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/fxml/event.fxml"));
+        loader.setController(new EventController(eventP, true));
+
+        JFXDecorator decorator;
+
+        try {
+            decorator = new JFXDecorator(registerStage, loader.load(), false, false, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(decorator, 450, 450);
+
+
+        scene.getStylesheets().addAll(
+                getClass().getResource("/gui/css/global.css").toExternalForm()
+        );
+        registerStage.initModality(Modality.WINDOW_MODAL);
+        registerStage.initOwner(ContextHandler.getContext().getStage("primary"));
+        registerStage.setScene(scene);
+        registerStage.show();
     }
 }
